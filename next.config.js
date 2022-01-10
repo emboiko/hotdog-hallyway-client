aeabc9f70d057bcebdc64e9311b09b54cfd09372
Emboiko <ed@emboiko.com>
2022-01-10 02:43:35 -0500

    config vars

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
-require("dotenv").config()
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config()
+}
+
 const path = require("path")
 const Dotenv = require('dotenv-webpack');
+
 
 const nextConfig = {
   webpack(config) {
     // Root alias for webpack
     // config.resolve.alias["~"] = path.join(__dirname, "/")
+
+    // Load system env vars for production
     config.plugins.push(new Dotenv({
       path: path.join(__dirname, ".env"),
       systemvars: true
@@ -13,8 +19,9 @@ const nextConfig = {
     return config
   },
   env: {
-    NEXT_PUBLIC_BACKEND_URL: process.env.BACKEND_URL
+    BACKEND_URL: process.env.BACKEND_URL
   }
 }
+
 
 module.exports = nextConfig
