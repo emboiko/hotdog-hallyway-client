9f52c7dd442497f657277d31f224582fadba4ece
Emboiko <ed@emboiko.com>
2022-01-09 22:31:03 -0500

    initial commit

---
+++ components/example.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import styled from "styled-components"
+
+const ExampleDiv = styled.div`
+  background: red;
+`
+
+const ExampleComponent = () => {
+  return (
+    <ExampleDiv>
+      Example Component
+    </ExampleDiv>
+  )
+}
+
+export default ExampleComponent
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Example from "../components/example"
+
+const Landing = () => {
+  return (
+    <>
+      Hotdog Hallway
+      <Example/>
+    </>
+  )
+}
+
+export default Landing
