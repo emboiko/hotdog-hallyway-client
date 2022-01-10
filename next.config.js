919e20b74110110716e143b54a63d28e37b7ccbc
Emboiko <ed@emboiko.com>
2022-01-10 02:09:00 -0500

    config var tests

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
-if (process.env.NODE_ENV === "development") {
-  const webpack = require('webpack')
+// if (process.env.NODE_ENV === "development") {
+//   const webpack = require('webpack')
+//   module.exports = {
+//     webpack(config) {
+//           const { parsed: env } = require('dotenv').config()
+//           config.plugins.push(new webpack.EnvironmentPlugin(env))
+//           return config
+//       }
+//   }
+// } else {
+  // require("dotenv").config()
+  const Dotenv = require("dotenv-webpack")
+  // const path = require("path")
   module.exports = {
     webpack(config) {
-          const { parsed: env } = require('dotenv').config()
-          config.plugins.push(new webpack.EnvironmentPlugin(env))
-          return config
-      }
+      config.plugins.push(new Dotenv({ systemvars: true }))
+      return config
+    }
   }
-}
+// }
