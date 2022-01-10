97bad907d7953cb8aa6aa84a246bcca8e4148a57
Emboiko <ed@emboiko.com>
2022-01-10 03:43:37 -0500

    add another log

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ if (process.env.NODE_ENV === "development") {
 const path = require("path")
 const Dotenv = require('dotenv-webpack');
 
+console.log(process.env.BACKEND_URL)
+
 const nextConfig = {
   webpack(config) {
     // Root alias for webpack
